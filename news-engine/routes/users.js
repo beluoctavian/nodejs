adbeb06638175110b78ef6baffc1e89a47659fc5
Belu Octavian <belu.octavian@yahoo.com>
2016-05-26 17:23:24 +0300

    Users login/singup routes

--- news-engine/routes/users.js
+++ news-engine/routes/users.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
 module.exports = router;
+
+module.exports = function(passport){
+  /* GET login page. */
+  router.get('/', function(req, res) {
+    // Display the Login page with any flash message, if any
+    res.render('index', { message: req.flash('message') });
+  });
+
+  /* Handle Login POST */
+  router.post('/login', passport.authenticate('login', {
+    successRedirect: '/home',
+    failureRedirect: '/',
+    failureFlash : true
+  }));
+
+  /* GET Registration Page */
+  router.get('/signup', function(req, res){
+    res.render('register',{message: req.flash('message')});
+  });
+
+  /* Handle Registration POST */
+  router.post('/signup', passport.authenticate('signup', {
+    successRedirect: '/home',
+    failureRedirect: '/signup',
+    failureFlash : true
+  }));
+
+  return router;
+};
