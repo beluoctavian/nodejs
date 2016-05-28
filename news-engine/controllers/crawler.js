beb684acc95c5f4a7b38fea923c27e451a7ecae2
Stanescu Leonard <Stanescu Leonard>
2016-05-28 12:57:26 +0300

    edited crawler.js

--- news-engine/controllers/crawler.js
+++ news-engine/controllers/crawler.js
@@ -5,43 +5,46 @@ var URL = require('url-parse');
 var webpage = "http://www.news.ro";
 
 var links = [];
+var categories = [];
 var titles = [];
 var news_list = [];
 
 request(webpage, function(error, response, body) {
 
-    if(response.statusCode === 200) {
+    var $ = cheerio.load(body);
 
-        var $ = cheerio.load(body);
+    $('.container-img').each(function() {
 
-        $('.container-img').each(function(){
+        var data = $(this);
 
-            var data = $(this);
+        var link = data.children().first().attr('href');
+        links.push('http://www.news.ro' + link);
 
-            var link = data.children.first().attr('href');
-            links.push('http://www.news.ro' + link);
+        var index = 1;
+        var category = '';
+        while (link[index] != '/') {
+            category = category + link[index];
+            index = index + 1;
+        }
+        categories.push(category);
 
-            var title = data.children.first().attr('title');
-            titles.push(title);
+        var title = data.children().first().attr('title');
+        titles.push(title);
 
-            var webpage2 = 'http://www.news.ro' + link;
+        var webpage2 = 'http://www.news.ro' + link;
 
-            request(webpage2, function(error, response, body) {
+        request(webpage2, function(error, response, body) {
 
-                if(response.statusCode === 200) {
+            var $2 = cheerio.load(body);
 
-                    var $2 = cheerio.load(body);
+            $2('.article-content').filter(function(){
 
-                    $2('.article-content').filter(function(){
+                var data2 = $(this);
 
-                        var data2 = $(this);
+                var news = data2.children('p').text();
+                news_list.push(news);
 
-                        var news = data2.children('p');
-                        news_list.push(news);
-
-                    })
-                }
-            });
-        })
-    }
+            })
+        });
+    })
 });
